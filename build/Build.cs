e7f6ba83fcea090de114e69496b5637845e1a598
LolghElmo <ghaithomo@gmail.com>
2026-04-07 18:51:10 +0300

    added nuke build

--- build/Build.cs
+++ build/Build.cs
@@ -14,7 +14,7 @@ using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [GitHubActions(
-    "FT CI",
+    "CI",
     GitHubActionsImage.UbuntuLatest,
     On = new[] { GitHubActionsTrigger.Push },
     InvokedTargets = new[] { nameof(Compile) },
